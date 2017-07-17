ca2af57e53cc6ae0de0d341af8127a3fe38cbed8
dimagixcart1223 <dimahovver@gmail.com>
2017-07-17 23:17:13 +0300

    Update helpdialog.cpp

--- client/helpdialog.cpp
+++ client/helpdialog.cpp
@@ -19,14 +19,14 @@ void CHelpDialog::Draw()
 	rect.left		= 10;
 	rect.bottom		= rect.top + 16;
 
-	pDefaultFont->RenderText("--- SA:CM Help ---",rect,0xFFFFFFFF); rect.top += 16; rect.bottom += 16;
-	pDefaultFont->RenderText("F1: Display this help dialog",rect,0xFFFFFFFF); rect.top += 16; rect.bottom += 16;
-	pDefaultFont->RenderText("TAB: Display the scoreboard",rect,0xFFFFFFFF); rect.top += 16; rect.bottom += 16;
-	pDefaultFont->RenderText("F4: Allows you to change class next time you respawn",rect,0xFFFFFFFF); rect.top += 16; rect.bottom += 16;
-	pDefaultFont->RenderText("F5: Show bandwidth statistics",rect,0xFFFFFFFF); rect.top += 16; rect.bottom += 16;
-	pDefaultFont->RenderText("F7: Toggle the chat box",rect,0xFFFFFFFF); rect.top += 16; rect.bottom += 16;
-	pDefaultFont->RenderText("F8: Take a screenshot",rect,0xFFFFFFFF); rect.top += 16; rect.bottom += 16;
-	pDefaultFont->RenderText("F9: Toggle the deathwindow",rect,0xFFFFFFFF); rect.top += 16; rect.bottom += 16;
-	pDefaultFont->RenderText("T/F6: Allows you to enter a chat message",rect,0xFFFFFFFF); rect.top += 16; rect.bottom += 16;
+	pDefaultFont->RenderText("--- SA:CM 0.3.8 ---",rect,0xFFFFFFFF); rect.top += 16; rect.bottom += 16;
+	pDefaultFont->RenderText("F1: Помощь",rect,0xFFFFFFFF); rect.top += 16; rect.bottom += 16;
+	pDefaultFont->RenderText("TAB: Меню",rect,0xFFFFFFFF); rect.top += 16; rect.bottom += 16;
+	pDefaultFont->RenderText("F4: Позволяет менять класс в следующий раз",rect,0xFFFFFFFF); rect.top += 16; rect.bottom += 16;
+	pDefaultFont->RenderText("F5: Статистика",rect,0xFFFFFFFF); rect.top += 16; rect.bottom += 16;
+	pDefaultFont->RenderText("F7: Скрыть/Видить чат",rect,0xFFFFFFFF); rect.top += 16; rect.bottom += 16;
+	pDefaultFont->RenderText("F8: Зделать скриншот",rect,0xFFFFFFFF); rect.top += 16; rect.bottom += 16;
+	pDefaultFont->RenderText("F9: Переключить deathwindow",rect,0xFFFFFFFF); rect.top += 16; rect.bottom += 16;
+	pDefaultFont->RenderText("T/F6: Писать в чат",rect,0xFFFFFFFF); rect.top += 16; rect.bottom += 16;
 	pDefaultFont->RenderText("Recruit Key: Enter vehicle as passenger",rect,0xFFFFFFFF); rect.top += 16; rect.bottom += 16;
 }
